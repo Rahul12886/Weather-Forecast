ac6f4aaafd7e9650537fa076f49f1cc3ab2528c2
Rahul12886 <rahulrathoree844124@gmail.com>
2026-05-24 10:55:30 +0530

    Fixed GitHub Pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,13 @@ import App from './App.jsx';
 import { WeatherProvider } from './context/WeatherContext.jsx';
 import './styles.css';
 
+const redirect = sessionStorage.redirect;
+delete sessionStorage.redirect;
+
+if (redirect && redirect !== location.href) {
+  history.replaceState(null, null, redirect);
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>
